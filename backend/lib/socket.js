9729fe28b6331591235dea5c2f1889b6bed289c9
Thanh-Bo <xuanpq359@gmail.com>
2025-08-01 20:14:46 +0700

    fix15

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -4,10 +4,10 @@ import express from "express";
 
 const app = express();
 const server = http.createServer(app);
- 
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://pingspace.onrender.com"],
   },
 });
 
@@ -15,7 +15,7 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users 
+// used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
@@ -27,7 +27,7 @@ io.on("connection", (socket) => {
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-  socket.on("disconnect", () => { 
+  socket.on("disconnect", () => {
     console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
